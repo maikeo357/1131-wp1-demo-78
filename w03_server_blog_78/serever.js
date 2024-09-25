efac8b470875de0233d6f66448ff6f85c287b62e
maikeo357 <212410095@o365.tku.edu.tw>
2024-09-25 19:39:09 +0800

    W03-P2: Create wp1_demo_78 database with a table blog_78, and put 2 data into blog_78

---
+++ w03_server_blog_78/serever.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+app.use('/', (res, res, next)=>{res.setEncoding('Mumu Hsiao, 212410178')})
+
+const port = process.env.
+PORT || 3000
+app.listen(port, () => {
+    console.log(`Server
+        running on port ${port}`)
+})
---
+++ w03_server_blog_78/utils/data/blog_78.sql
@@ -0,0 +1,14 @@
+create_table blog_78 (
+    id int not null primary key,
+    title varchar(255),
+    descrip text,
+    category varchar(255),
+    img text,
+    remote_img text
+)
+
+insert into blog_78(id, title, description, category, img, remote_img)
+values
+(1, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', './images/photo-1.jpg', ''),
+(2, 'travel to paris', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'travel', './images/photo-2.jpg', ''),
+(3, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', './images/photo-3.jpg', '')
